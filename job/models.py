fdb2e9c77941221cff9113a28a028ca876ae8283
IbrahimSamir0 <ibrahim20208002@fci.helwan.edu.eg>
2022-10-13 17:24:56 +0200

    add job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,27 @@
+from email.policy import default
+from enum import auto
+from pydoc import describe
+from random import choices
+from statistics import mode
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
+JOB_TYPE=(
+    ('Full Time','Full Time'),
+    ('Part Time','Part Time'),
+)
+
 # Create your models here.
+class Job(models.Model):
+    # title
+    title = models.CharField(max_length=100)    
+    # location
+    job_type =models.CharField(max_length=20,  choices=JOB_TYPE)
+    describtion = models.TextField(max_length=1000)
+    published_at= models.DateTimeField(auto_now=True)
+    vacancy =models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=0)
+
+    def __str__(self) :
+        return self.title
---
+++ outline.md
@@ -0,0 +1,30 @@
+Job : 
+    -Title
+    -Location
+    -Job type
+    -Description
+    -published at
+    -vacancy
+    -Salary
+    -category
+    -exeperince
+
+    -apply job
+    -Post job
+    
+blog :
+    -title
+    -description
+    -created_at
+    -category
+    -tags
+    -Author
+
+    -search
+    -comment
+    -recent_post
+
+Contact
+Home
+
+login
